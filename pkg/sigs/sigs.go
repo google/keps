4ce605e4dc3a6e7e72b7e8734456ae51884d0d1d
caleb miles <caselim@gmail.com>
2018-09-27 13:01:49 -0700

    implements extracting SIG and Subproject information from a path string

--- pkg/sigs/sigs.go
+++ pkg/sigs/sigs.go
@@ -1,6 +1,9 @@
 package sigs
 
 import (
+	"path/filepath"
+	"strings"
+
 	"github.com/calebamiles/keps/pkg/sigs/internal/generated"
 )
 
@@ -13,9 +16,48 @@ func Exists(s string) bool {
 }
 
 func ExtractNameFromPath(p string) string {
+	sigName, _ := extractSIGandSubprojectNamesFromPath(p)
 
+	return sigName
 }
 
 func ExtractSubprojectNameFromPath(p string) string {
+	_, subprojectName := extractSIGandSubprojectNamesFromPath(p)
 
+	return subprojectName
+}
+
+func extractSIGandSubprojectNamesFromPath(p string) (string, string) {
+	var sigName string
+	var subprojectName string
+
+	sigs := map[string]bool{}
+	subprojects := map[string]bool{}
+
+	components := strings.Split(p, string(filepath.Separator))
+	for i, possibleSIG := range components {
+		var possibleSubproject string
+		if i < len(components) - 1  {
+			possibleSubproject = components[i + 1]
+		}
+
+		if generated.SIGSet[possibleSIG] {
+			sigs[possibleSIG] = true
+			sigName = possibleSIG
+		}
+
+		if generated.SIGSubprojectMapping[possibleSIG][possibleSubproject] {
+			subprojects[possibleSubproject] = true
+			subprojectName = possibleSubproject
+		}
+	}
+
+	switch {
+	case len(sigs) > 1:
+		sigName = "" // couldn't determine SIG uniquely
+	case len(subprojects) > 1:
+		subprojectName = "" // couldn't determine subproject uniquely
+	}
+
+	return sigName, subprojectName
 }
--- pkg/sigs/sigs_test.go
+++ pkg/sigs/sigs_test.go
@@ -28,20 +28,33 @@ var _ = Describe("the SIGs helper package", func() {
 
 	Describe("Exists()", func() {
 		It("returns whether a specific SIG exists", func() {
+			upstreamList := fetchUpstreamSIGNames()
 
+			for _, s := range upstreamList {
+				Expect(sigs.Exists(s)).To(BeTrue())
+			}
 		})
 	})
 
 	Describe("ExtractNameFromPath()", func() {
 		Context("when the SIG exists", func() {
 			It("extracts the SIG name from a given path", func() {
-
+				p := "content/sig-node/kubelet"
+				Expect(sigs.ExtractNameFromPath(p)).To(Equal("sig-node"))
 			})
 		})
 
 		Context("when the SIG does not exist", func() {
 			It("returns an empty string", func() {
+				p := "content/sig-not-real/kubelet"
+				Expect(sigs.ExtractNameFromPath(p)).To(BeEmpty())
+			})
+		})
 
+		Context("when there are multiple SIG names in a given path", func() {
+			It("returns an empty string", func() {
+				p := "content/sig-node/sig-architecture/kublet"
+				Expect(sigs.ExtractNameFromPath(p)).To(BeEmpty())
 			})
 		})
 	})
@@ -49,20 +62,23 @@ var _ = Describe("the SIGs helper package", func() {
 	Describe("ExtractSubprojectNameFromPath()", func() {
 		Context("when the subproject is immediately nested under the SIG", func() {
 			It("extracts the subproject name from a given path", func() {
-
+				p := "content/sig-node/kubelet"
+				Expect(sigs.ExtractSubprojectNameFromPath(p)).To(Equal("kubelet"))
 			})
 
 		})
 
 		Context("when the subproject is not immediately nested under the SIG", func() {
 			It("returns an empty string", func() {
-
+				p := "content/sig-node/some-dir/kubelet"
+				Expect(sigs.ExtractSubprojectNameFromPath(p)).To(BeEmpty())
 			})
 		})
 
 		Context("when the subproject is not owned by the SIG it is nested under", func() {
 			It("returns an empty string", func() {
-
+				p := "content/sig-networking/kubelet"
+				Expect(sigs.ExtractSubprojectNameFromPath(p)).To(BeEmpty())
 			})
 		})
 	})
