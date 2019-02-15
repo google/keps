1d074caf9872b4462ba5e8cf522ae00959acbfad
caleb miles <caselim@gmail.com>
2019-02-14 19:03:42 -0800

    adds super basic handling of a GitHub user auth token
    ...reading it from disk every time the token is requested

---
+++ pkg/changes/auth/arbitrary_basic_auth_username.go
@@ -0,0 +1,5 @@
+package auth
+
+const (
+	ArbitraryUsername = "ossKEPtool"
+)
---
+++ pkg/changes/auth/token_provider.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"io/ioutil"
+)
+
+func NewProvideTokenFromPath(p string) (TokenProvider, error) {
+	var value = func() (string, error) {
+		tokenBytes, err := ioutil.ReadFile(p)
+		if err != nil {
+			return "", err
+		}
+
+		return string(tokenBytes), nil
+	}
+
+	t := &tokenFromPath{
+		value: value,
+	}
+
+	return t, nil
+}
+
+
+type TokenProvider interface {
+	Value() (string, error)
+}
+
+type tokenValueFunc func() (string, error)
+
+type tokenFromPath struct {
+	value tokenValueFunc
+}
+
+func (t *tokenFromPath) Value() (string, error) {
+	return t.value()
+}
+
